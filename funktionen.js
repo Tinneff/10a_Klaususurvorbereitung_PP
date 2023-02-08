b8a2944c515955d1aa7bda82201a687b00f8db04
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-02-08 08:49:49 +0100

    Add files via upload

---
+++ funktionen.js
@@ -0,0 +1,38 @@
+/* DATENSTRUKTUREN */
+
+/* Primitives */
+
+
+
+// Wertzuweisung / Assignment
+
+
+
+
+/* Arrays */
+// Navigation über INDEX
+
+
+/* Daten-Objekte */
+// Navigation über Bezeichner (keys)
+
+/
+
+ /*DOM*/
+// DOM  --> Modell der HTMl-Struktur
+// DOM --> Document Object Model
+// DOM --> nach dem Parsen / Laden der HTML-Seite  
+
+
+/* ENTSCHEIDUNGSSTRUKTUREN | CONTROL FLOW*/
+
+
+ // https://github.com/01-PP/03_Entscheidungsstrukturen
+
+/* FUNKTIONEN */
+
+
+// rechner();
+
+
+
